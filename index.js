32624a7e628fc8863e3c17a098312e7433568188
Ivy Kathrina <88631468+newbiedev08@users.noreply.github.com>
2021-10-16 19:32:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Hello Winc Academy")
+let name='Ivy';
+console.log ('Ivy')
+let name='Ivy';
+console.log(name);
+2 + 2
+let x = 5;
+let y =5;
+let z= x + y;
+console.log(z);
+let a = 10;
+let b = 5;
+let c = a-b;
+console.log(c);
+let d = 3;
+let e = 3;
+let f= 3 * 3;
+console.log(f);
+let g= 8;
+let h= 3;
+let i= 8%3;
+console.log(i);
+let age ='48';
+console.log(age);
+console.log(typeof 37);
